4b153eda7712c929255a236d7ec5fe4d67577384
N7 GIL <gilbertoaescobedo@gmail.com>
2022-07-10 19:55:17 -0700

    worked on a css layout on where the data will be going. 1st attempt of fetching data from api

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,31 @@
-function getData() { 
+let todayMoment = moment();
 
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}")
-        .then(response => {
-            return response.json()
-        })
-        .then((data) => {
-            console.log(data)
-        })
+let cityDate = $("#date")
+cityDate.text(todayMoment.format("MM/DD/YY"));
+
+
+
+
+
+
+function getWeatherApi() {
+
+    let city = "lat, lon";
+    let currentTemp = "";
+    let forecastTemp = "";
+
+// fetching: city, current temperature, wind speed, humidity, and UV Index
+// fetching forecast: daily temp, daily wind speed, and daily humidity
+    let requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&current.temp&current.wind_speed&hourly.humidity&hourly.uv&daily.temp&daily.wind_speed&daily.humidity&appid={2be22b86b9aea046815ddb323663aa59}"
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(data) {
+        const weatherData = data.results;
+        console.log(weatherData);
+    })
 }
 
-(getData)
